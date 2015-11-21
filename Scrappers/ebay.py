2868d65b8b3daa2e9e3a3f5a7045188fe2c2a60c
Jerry Crowley <jeremiah.c.crowley@gmail.com>
2015-11-21 18:21:04 -0500

    Added mock ebay scrapper

--- Scrappers/ebay.py
+++ Scrappers/ebay.py
@@ -1,9 +1,14 @@
-import feedparser
+import ebaysdk
+from ebaysdk.finding import Connection as Finding
+from ebaysdk.exception import ConnectionError
 
 def main():
-    xml = feedparser.parse("http://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313.TR10.TRC2.A0.H0.Xxeon.TRS2&_nkw=xeon&_sacat=0&_rss=1")
-
-    for post in xml.entries:
-        print post.title
+    try:
+        api = Finding(appid="Jeremiah-c9e4-41cd-93a3-db5086f58faf")
+        response = api.execute('findItemsAdvanced', {'keywords': '3930k'})
+        print(response.dict())
+    except ConnectionError as e:
+        print(e)
+        print(e.response.dict())
 
 main()
