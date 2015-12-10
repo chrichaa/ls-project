3535365313f3adc8f4783a4c2567acbf265dc3d3
Christina <cc3561@nyu.edu>
2015-12-10 15:29:20 -0500

    updated views with login/reg

--- mysite/project/urls.py
+++ mysite/project/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from project import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^register', views.register, name="register"),
+    url(r'^dashboard', views.dashboard, name="dashboard")
 )
--- mysite/project/views.py
+++ mysite/project/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'project/index.html')
+
+def register(request):
+    return render(request, 'project/register.html')
+
+def dashboard(request):
+    return render(request, 'project/dashboard.html')
