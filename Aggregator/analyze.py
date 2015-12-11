9742cf9e10f44c37aa1378f9aa508774656364cb
EC2 Instance <jeremiah.c.crowley@gmail.com>
2015-12-11 01:27:49 +0000

    analyze done

--- Aggregator/analyze.py
+++ Aggregator/analyze.py
@@ -1,27 +1,39 @@
+def avg(l):
+	s=0
+	for s in range(0, len(l)):
+		l[s]=float(l[s])
+	try:
+    		return sum(l, 0.0) / len(l)
+	except ZeroDivisionError:
+		print("List is empty!")
+
 def most_common(lst):
-    return max(set(lst), key=lst.count)
+	return max(set(lst), key=lst.count)
 
 def average(filename):
 	try:
-		print(filename)
+		#print(filename)
+		with open(filename) as f:
+			content=f.readlines()
+		average=avg(content)
 		
-
-	except IOError:
-		print("An error has occured, does the file exist?")
+		print("The average "+filename[:-4]+" is "+str(average))
+	except(IOError):
+		print("An error has occured, does the file "+filename+" exist?")
 
 def mostcommon(filename):
 	try:
-		print(filename)
+		#print(filename)
 		with open(filename) as f:
    			content = f.readlines()
 		mostCommon=most_common(content)
-		print ("Most common %s is %s", filename, mostCommon)
+		print ("Most common "+filename[:-4]+" is "+mostCommon[:-1])
 
 	except IOError:
-		print("ERROR")
+		print("ERROR, does the file exist?")
 
 def main():
-        print("Hello world")
+       # print("Hello world")
         average("timestat.txt")
         mostcommon("locationstat.txt")
         mostcommon("keywordstat.txt")
