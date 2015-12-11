1bae277725bbd70dac4bd5ba3839c411caa67975
Jerry Crowley <jeremiah.c.crowley@gmail.com>
2015-12-11 17:20:28 -0500

    Fixed Cities Dictionary

--- Aggregator/citiesDic.py
+++ Aggregator/citiesDic.py
@@ -1,14 +1,20 @@
+import json
+
 def splitter(c):
-	cities_dict={}
-	for x in range(len(c))
-		temp=c[x].split("/\">")
-		cities_dict[temp[0]=temp[1]
-	return cities_dict
+    cities_dict={}
+
+    for x in range(len(c)):
+        temp = c[x].split("/\">")
+        cities_dict[temp[0]] = temp[1]
+    return cities_dict
 
 def main():
-	#print("hello world")
-	with open("uscities.txt") as f:
-		content=f.readlines()
-	dictionary=splitter(content)
+    with open("uscities.txt") as f:
+        content = f.readlines()
+	
+    dictionary = splitter(content)
+                    
+    print json.dumps(dictionary, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
+
 if __name__ == "__main__":
     main()
